ea06ab28f497961bf95b864ab09632fd026f5eea
Ricardo Cruz <45740518+ricardomcruz@users.noreply.github.com>
2021-11-02 09:08:13 +0000

    complete exercises until serve json

--- myApp.js
+++ myApp.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var app = express();
 
-console.log("Hello world");
+/* app.get('/', (req, res) => {
+  res.send("Hello Express");
+});
+ */
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.get('/', (req, res) => {
+  res.sendFile(absolutePath);
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
-
-
-
-
-
+app.get('/json', (req, res) => {
+  res.json({"message": 'Hello json'})
+});
 
 
 
